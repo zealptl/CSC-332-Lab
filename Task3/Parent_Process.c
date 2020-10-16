bbfc40053e98310ec9de6baf6640b2e5b0dfd3c0
zealptl <zealpatel06@gmail.com>
2020-10-15 23:57:40 -0400

    add images

--- Task3/Parent_Process.c
+++ Task3/Parent_Process.c
@@ -8,12 +8,14 @@
 int main()
 {
   pid_t pd = fork();
-  if (pd  == 0)
+  if (pd == 0)
   {
     printf("I am child 1. My pid is %d\n", getpid());
     execl("./Process_P1", "./Process_P1", NULL);
   }
   wait(NULL);
+
+  sleep(3);
 
   pd = fork();
   if (pd == 0)
@@ -21,6 +23,7 @@ int main()
     printf("I am child 2. My pid is %d\n", getpid());
     execl("./Process_P2", "./Process_P2", NULL);
   }
+
   wait(NULL);
   exit(0);
 }
